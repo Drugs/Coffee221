6ca69bfb5c33a85a0f841ac6f6feb4a00495d84f
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2022-08-22 19:37:45 -0300

    Create cadastropag.php

---
+++ cadastro/cadastropag.php
@@ -0,0 +1,16 @@
+<?php
+include "../include/header.php";
+?>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="cartao central"></div>
+            </div>
+        </div>
+    </div>
+
+<?php
+include "../include/footer.php";
+?>
