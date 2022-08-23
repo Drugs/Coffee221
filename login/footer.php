5140e72a766696db486718d7ff7568c7c44bae58
Bianca Nogueira <biancanogueiradasilva12@gmail.com>
2022-08-23 18:04:30 -0300

    nao

--- login/footer.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript" src="/_assets/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="/_assets/js/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="/_assets/js/script.js"></script>
-</body>
-
-</html>
