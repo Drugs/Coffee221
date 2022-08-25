1c9d8c9d519cc51fbd5856531c734cda0f59bce8
Jonas Carvalho <jonasgabrielsc@gmail.com>
2022-08-25 08:48:03 -0300

    Debug

--- Produtodetalhes/produto.php
+++ Produtodetalhes/produto.php
@@ -25,20 +25,19 @@ include "head.php";
 
         <div class="container-lg">
             <div class="row">
-
                 <div class="col-md-6">
                     <h2>
                         <center> Camisa swile <i class="bi bi-bookmark-plus"></i></center>
                     </h2>
                     <div id="cafedama" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                            <div class="carousel-item">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
                                 <img src="../Imagens/camisa1.png" class="d-block w-100" alt="...">
                             </div>
                             <div class="carousel-item">
                                 <img src="../Imagens/camisa2.png" class="d-block w-100" alt="...">
                             </div>
-                            <div class="carousel-item active">
+                            <div class="carousel-item">
                                 <img src="../Imagens/Camisa3.png" class="d-block w-100" alt="...">
                             </div>
                         </div>
@@ -91,11 +90,10 @@ include "head.php";
             </div>
         </div>
     </div>
-    </div>
 
     <div class="album py-5 bg-light">
         <div class="container">
-        <h3>Sugestão</h3>
+            <h3>Sugestão</h3>
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                 <div class="col">
                     <div class="card shadow-sm">
@@ -105,7 +103,9 @@ include "head.php";
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
                                 </div>
-                                <small class="text-muted"><h5>Camisa do café</h5></small>
+                                <small class="text-muted">
+                                    <h5>Camisa do café</h5>
+                                </small>
                             </div>
                         </div>
                     </div>
@@ -118,26 +118,30 @@ include "head.php";
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
                                 </div>
-                                <small class="text-muted"><h5>Camisa do café</h5></small>
+                                <small class="text-muted">
+                                    <h5>Camisa do café</h5>
+                                </small>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card shadow-sm">
-                        <img src="../Imagens/caf2.png" alt="" >
+                        <img src="../Imagens/caf2.png" alt="">
 
                         <div class="card-body">
 
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
                                 </div>
-                                <small class="text-muted"><h5>Camisa do café</h5></small>
+                                <small class="text-muted">
+                                    <h5>Camisa do café</h5>
+                                </small>
                             </div>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
 
         </div>
