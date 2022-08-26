bf65a9a716453993c2b2a387d59665c49b4aa4bf
Bianca Nogueira <biancanogueiradasilva12@gmail.com>
2022-08-26 17:35:34 -0300

    1

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -2,7 +2,7 @@
 $title="Cadastro";
 include "../include/header.php";
 ?>
-<body>
+<body class="bianca"> 
 	<div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
