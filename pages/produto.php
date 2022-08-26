1be89437c35c580b616a87235cc229adcfa31421
Bianca Nogueira <biancanogueiradasilva12@gmail.com>
2022-08-26 16:31:20 -0300

    f

--- pages/produto.php
+++ pages/produto.php
@@ -3,7 +3,7 @@ $title = "Produto";
 include "../include/header.php";
 ?>
 
-<body>
+<body class="bianca">
     <div class="container">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
             <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
