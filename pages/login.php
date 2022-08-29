fafd8b3d9679e9b26b63bf3d0cf36795281bda33
Bianca Nogueira <biancanogueiradasilva12@gmail.com>
2022-08-29 17:20:56 -0300

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -34,14 +34,10 @@ if(isset($_GET['login'])and $_GET['login'] == 'send'){
                 <div class="card bg-dark " style="border-radius: 1rem;" id="card">
                     <div class="card-body p-5 text-center">
                         <h2 style="color:white;">LOGIN</h2>
-                    <form action="valida.php">
+                    <form action="">
                         <div class="form-floating mt-3">
-                            <input type="text" class="form-control" id="floatingPassword" placeholder="Password">
+                            <input type="text" class="form-control" id="floatingPassword" placeholder="Password" name="email">
                             <label for="floatingPassword">Digite seu Username</label>
-                        </div>
-                        <div class="form-floating mt-2">
-                            <input type="email" class="form-control" id="floatingPassword" placeholder="Password"  name="email">
-                            <label for="floatingPassword">Digite seu Email</label>
                         </div>
                         <div class="form-floating mt-2">
                             <input type="password" class="form-control" id="floatingPassword" placeholder="Password"  name="senha">
@@ -49,7 +45,7 @@ if(isset($_GET['login'])and $_GET['login'] == 'send'){
                             
                         </div>
                         <div class="mt-2 d-grid gap-2">
-                        <button type="submit" value='send' name='login' class="btn btn-outline-light" href="../home.php">Entrar</button>
+                        <button type="submit" value='send' name='login' class="btn btn-outline-light">Entrar</button>
                                 <p style="color:white;">Ainda não tem uma conta?<a href="usuario_cadastro.php"> Clique aqui</p>
                         </div>
                     </form>
