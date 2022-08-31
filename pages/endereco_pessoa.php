8559fe576314ba7dd7f0f240ea543748eddb814f
ana16clara <111700812+ana16clara@users.noreply.github.com>
2022-08-31 14:39:25 -0300

    pagina usuario

--- pages/endereco_pessoa.php
+++ pages/endereco_pessoa.php
@@ -3,7 +3,7 @@ include'../include/header.php';
 include'../include/database.php';
 
 $consulta="SELECT *,endereco.cidade from pessoa 
-join endereco on pessoa.id_pessoa=endereco.fk_id_pessoa where pessoa.id_pessoa=''
+join endereco on pessoa.id_pessoa=endereco.fk_id_pessoa where pessoa.id_pessoa='2'
 ";
 
 echo $consulta;
@@ -13,7 +13,7 @@ echo $consulta;
   <div class='container'>
      <div class='row'>  
     <h1 class='text-center'>endereço </h1>
-      <a class="btn mb-3 btn-warning float-start" href="cadastro_usuario.php">cadastre-se</a>
+      <a class="btn mb-3 btn-warning float-start" href="cadastro_endereco.php">cadastre-se</a>
       <div class="col-12">
                <table class="mt-3 table table-dark" style="box-shadow:10px 10px 50px  #000000,10px 10px 10px  #000000;">
                 <thead class="table table-dark table-striped">
@@ -23,6 +23,7 @@ echo $consulta;
                  <th scope ="col">Rua</th>
                  <th scope ="col">Avenida</th>
                  <th scope ="col">numero</th>
+                 <th scope ="col">id_pessoa</th>
                  <th scope ="col">CEP</th>
                  <th scope ="col">Estado</th>
                  <th scope ="col">Complemento</th>
@@ -43,13 +44,14 @@ echo $consulta;
      <td>{$endereco['rua']}</td>
      <td>{$endereco['avenida']}</td>
      <td>{$endereco['numero']}</td>
+     <td>{$endereco['id_pessoa']}</td>
      <td>{$endereco['cep']}</td>
      <td>{$endereco['estado']}</td>
      </td>
      <td>{$endereco['complemento']}</td>
-     <td><a class='btn btn-warning' href='.php?var={$endereco['id_pessoa']}'>Atualizar</a> 
+     <td><a class='btn btn-warning' href='.php?var={$endereco['id_endereco']}'>Atualizar</a> 
      </td>
-     <td><a class='btn btn-outline-warning' href='.php?var={$endereco['id_pessoa']}'>Deletar</a> </td>
+     <td><a class='btn btn-outline-warning' href='.php?var={$endereco['id_endereco']}'>Deletar</a> </td>
                </tr>";
                  }
                  
