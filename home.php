8edd577fe0d70776c18c2faa62bfb8079496995c
Samuel Goes <samuel.wesleimoreira@gmail.com>
2022-08-31 18:01:30 -0300

    sla

--- home.php
+++ home.php
@@ -3,9 +3,10 @@
 
 <head>
     <title>The COFFE'JOIN</title>
-    <?php
-include '../include/header.php';
-?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
+    
     <style>
         .feature-icon {
             display: inline-flex;
@@ -478,9 +479,12 @@ include '../include/header.php';
 
 
 
- <?php
-include '../include/footer.php';
-?>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
