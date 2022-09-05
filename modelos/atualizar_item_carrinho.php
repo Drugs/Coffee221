2cffb363e721a3d338ef969016e5b9865fb7c319
Bianca Nogueira <biancanogueiradasilva12@gmail.com>
2022-09-05 19:18:36 -0300

    5

--- modelos/atualizar_item_carrinho.php
+++ modelos/atualizar_item_carrinho.php
@@ -6,8 +6,8 @@ include'../include/database.php';
 $id=$_GET['var'];
 
 if(isset($_GET['submit']) and $_GET['submit']=='send'){
-    $consulta = "UPDATE item_de_carrinho SET  carrinho = '{$_GET['id_item_carrinho']}', id_produto = '{$_GET['fk_id_info_produto']}', 
-    quantidade = '{$_GET['quantidade']}' WHERE id_item_carrinho = {$id}";
+    $consulta = "UPDATE item_de_carrinho SET   id_produto = '{$_GET['fk_id_info_produto']}', carrinho={$_GET['fk_id_carrinho']}, 
+    quantidade = '{$_GET['quantidade']}' Join  produto On id_produto =fk_id_info_produto    WHERE id_item_carrinho = {$id}";
     echo $consulta;
     $query=mysqli_query ($con, $consulta);
       if($query){
@@ -17,9 +17,9 @@ if(isset($_GET['submit']) and $_GET['submit']=='send'){
       }
     }
 
-    $consulta="Select * From item_carrinho where id_item_carrinho = {$id}";
+    $consulta2="Select * From item_de_carrinho where id_item_carrinho = {$id}";
    // var_dump($consulta);
-    $query=mysqli_query($con, $consulta);
+    $query=mysqli_query($con, $consulta2);
     $result = mysqli_fetch_assoc($query);
 
 ?>
@@ -34,17 +34,19 @@ if(isset($_GET['submit']) and $_GET['submit']=='send'){
                     <div class="col-6">
                         <div class="col-12">
                             <div class="form-floating" style="margin-top: 1cm;">
-                            <input type="text" class="form-control" name='carrinho' value="<?php echo $result['id_item_carrinho'];?>">
-                                <label for="exampleFormControlInput1" class="form-label"><strong>ID do carrinho</strong></label>
-                            </div>
-                            <div class="form-floating">
                             <input type="text" class="form-control" name='id_produto' value="<?php echo $result['fk_id_info_produto'];?>">
                              <label for="exampleFormControlInput1" class="form-label"><strong>ID do produto </strong></label>
+                            </div>
+                            
+                            <div class="form-floating">
+                            <input type="text" class="form-control" name='carrinho' value="<?php echo $result['fk_id_carrinho'];?>">
+                                <label for="exampleFormControlInput1" class="form-label"><strong>ID do carrinho</strong></label>
                             </div>
                             <div class="form-floating" style="margin-bottom: 5mm;">
                                  <input type="text" class="form-control" name='quantidade' value="<?php echo $result['quantidade'];?>">
                                 <label for="exampleFormControlInput1" class="form-label"><strong>Quantidade</strong></label>
                             </div>
+
                             <input type="hidden" name="var" value="<?php echo $_GET['var'];?>" />
                             <div class="row">
                                 <div class="col-6">
