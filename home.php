b8d80889df371ea83158eec7694c6464c6ed9dab
Samuel Goes <samuel.wesleimoreira@gmail.com>
2022-09-05 14:59:06 -0300

    Update home.php

--- home.php
+++ home.php
@@ -40,6 +40,7 @@
 				</li>
 			</ul>
 
+<<<<<<< Updated upstream
 			<form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
 				<input type="search" class="form-control form-control-dark" placeholder="Procurar..." aria-label="Procurar">
 			</form>
@@ -53,6 +54,85 @@
 			}
 			?>
 			</div>
+=======
+<head>
+    <title>The COFFE'JOIN</title>
+ 
+    
+    <style>
+        .feature-icon {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            width: 4rem;
+            height: 4rem;
+            margin-bottom: 1rem;
+            font-size: 2rem;
+            color: #fff;
+            border-radius: 0.75rem;
+        }
+    </style>
+
+</head>
+
+<body >
+
+
+            <header class="p-3 bg-dark text-white">
+                
+                    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                                <use xlink:href="#bootstrap"></use>
+                            </svg>
+                        </a>
+
+                        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                            <h2><i class="bi bi-cup-hot"></i> </h2>
+
+                            <li><a href="#" class="nav-link px-2 text-warning">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">The COFFE'JOIN</font>
+                                    </font>
+                                </a></li>
+                            <li><a href="#" class="nav-link px-2 text-white">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Mais Sabores</font>
+                                    </font>
+                                </a></li>
+                            <li><a href="#" class="nav-link px-2 text-white">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Preços</font>
+                                    </font>
+                                </a></li>
+                            <li><a href="#" class="nav-link px-2 text-white">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Perguntas frequentes</font>
+                                    </font>
+                                </a></li>
+                            <li><a href="#" class="nav-link px-2 text-white">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Sobre</font>
+                                    </font>
+                                </a></li>
+                        </ul>
+
+                        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                            
+                            <input type="search" class="form-control form-control-dark" placeholder="Procurar..."
+                                aria-label="Procurar">
+                        </form>
+                        <?php
+                        if(isset($_SESSION['id_usu']) and  $_SESSION['id_usu'] != '' ){
+                           echo'<div class="text-end">';
+                            echo'<a class="btn btn-outline-light me-2" href="pages/logout.php"> Sair</a>';
+                        }else{
+                            echo'<div class="text-end">';
+                           echo'<a class="btn btn-outline-light me-2" href="pages/login.php">Login</a>';
+                        }
+                        ?>
+                        </div>
+>>>>>>> Stashed changes
             
             </header>
   
@@ -306,12 +386,7 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-        crossorigin="anonymous"></script>
+
 </body>
 
 </html>
