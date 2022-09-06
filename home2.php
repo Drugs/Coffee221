65011d36b6fabaf9218de59a1e6439ec523f93d5
Samuel Goes <samuel.wesleimoreira@gmail.com>
2022-09-06 16:26:01 -0300

    sla

--- home2.php
+++ home2.php
@@ -75,7 +75,28 @@ include './include/nave.php'
 				</div>
 			</div>
 		</div>
+<<<<<<< Updated upstream
 		
+=======
+
+<script>
+
+function rodinhas(id_info_prod){
+	console.log(id_info_prod)
+	var xmlhttp= new XMLHttpRequest();
+	xmlhttp.onredystatechange= function(){
+		if(this.readyState==4 && this.tatus==20){
+		console.log(this.responseText)
+		}
+	}
+	xmlhttp.open("GET", "pages/carrinhoinhu.php?id="+ id_info_prod)
+	xmlhttp.send();
+}
+
+</script>
+
+
+>>>>>>> Stashed changes
 		<div class="col-md-3">
 			<div class="card shadow-sm">
 				<img src="./Imagens/fototeste.jpg" heigth="">
