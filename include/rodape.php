63a64755e2e8a5db5670fd1402eedc838d96a037
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2022-09-05 22:10:48 -0300

    Create rodape.php

---
+++ include/rodape.php
@@ -0,0 +1,20 @@
+    <footer>
+        <section class="footer-1 d-flex justify-content-md-center align-items-md-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-muted d-flex justify-content-md-center align-items-md-center">
+                        <span>© Copyright 2022. All Rights Reserved. Last Update: 2022-09-04</span>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </footer>
+
+    <style>
+        .footer-1{
+            background-color: rgb(172, 162, 162);
+            margin-top: 80px;
+            height: 100px;
+        }
+    </style>
