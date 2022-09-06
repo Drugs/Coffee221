73e5dfb7c0a7ba34e31388b123c202601acbf8ac
Bianca Nogueira <biancanogueiradasilva12@gmail.com>
2022-09-06 17:34:25 -0300

    Boa sorte

--- pages/botanocarrinho.php
+++ pages/botanocarrinho.php
@@ -8,7 +8,18 @@
 		#	> Select carrinho onde pessoa == pessoa da sessão
 		#	> Criar um carrinho novo
         //$id_usu $id_pessoa
-		$consulta="INSERT INTO item_de_carrinho( fk_id_info_produto, fk_id_carrinho, quantidade) VALUES ('{$_GET['fk_id_info_produto']},'{$_GET['fk_id_carrinho']},'{$_GET['quantidade']}";
-		$query=mysqli_query($con, $consulta);
+	//	$consulta="INSERT INTO item_de_carrinho( fk_id_info_produto, fk_id_carrinho, quantidade) VALUES ('{$_GET['fk_id_info_produto']},'{$_GET['fk_id_carrinho']},'{$_GET['quantidade']}";
+		//$query=mysqli_query($con, $consulta);
+		
+     if(isset( )){ //login
+
+			if(){ //carrinho
+
+			}else{
+
+			}
+	 }else{
+    echo'<div class="alert alert-danger" role="alert">Você não está Logado!</div>';
+	 }
 	}
 ?>
