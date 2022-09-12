a1cfd189459fc36110cdf30e08396e8f2f2c77a4
ana16clara <111700812+ana16clara@users.noreply.github.com>
2022-09-12 16:41:27 -0300

    s

--- pages/pagina_usuario.php
+++ pages/pagina_usuario.php
@@ -22,91 +22,88 @@ if(isset($_GET['submit']) and $_GET['submit']=='send'){
 
 
 <body>
-    
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3"  href="../index.php">Coffee 221</a>
-      <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-     
-      <div class="navbar-nav">
-        <div class="nav-item text-nowrap">
-        <a class=" btn  btn-warning me-2"    href="logout.php">Desconectar</a>
-        </div>
-      </div>
-    </header>
-    
-    <div class="container-fluid">
-      <div class="row">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-          <div class="position-sticky pt-3">
-
-
-                <ul class="nav flex-column">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../index.php">
-                    <i class="bi bi-house"></i>
-                      Home
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                    <i class="bi bi-file-earmark-text"></i>
-                      Historico de compras
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                    <i class="bi bi-cart4"></i>
-                      Produtos no carrinho
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                    <i class="bi bi-person-circle"></i>
-                      Usuário
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="checkoutpage.php">
-                    <i class="bi bi-basket3"></i>
-                      Checkout
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                    <i class="bi bi-stack"></i>
-                      Integrations
-                    </a>
-                  </li>
-                </ul>
-            </nav>
-    
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-           
+        
+        <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+          <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3"  href="../index.php">Coffee 221</a>
+          <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+          <div class="navbar-nav">
+            <div class="nav-item text-nowrap">
+            <a class=" btn  btn-warning me-2"    href="logout.php">Desconectar</a>
+            </div>
           </div>
-    
-          <canvas width="1" height="1" style="display: block; width: 1px; height: 1px;"></canvas> 
-                  <svg class="bd-placeholder-img card-img-top" width="50%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Foto de usuário</text>
-                  </svg>
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                      
-                        <button type="button" onclick="document.location= 'atualizar_usuario.php '" class="btn btn-sm btn-warning">Atualizar</button>
+        </header>
+        
+        <div class="container-fluid">
+          <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+              <div class="position-sticky pt-3">
+
+                    <ul class="nav flex-column">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../index.php">
+                        <i class="bi bi-house"></i>
+                          Home
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="bi bi-file-earmark-text"></i>
+                          Historico de compras
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="bi bi-cart4"></i>
+                          Produtos no carrinho
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="bi bi-person-circle"></i>
+                          Usuário
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="checkoutpage.php">
+                        <i class="bi bi-basket3"></i>
+                          Checkout
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="bi bi-stack"></i>
+                          Integrations
+                        </a>
+                      </li>
+                    </ul>
+                </nav>
+        
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+              <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              </div>
+              <canvas width="1" height="1" style="display: block; width: 1px; height: 1px;"></canvas> 
+                      <svg class="bd-placeholder-img card-img-top" width="50%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Foto de usuário</text>
+                      </svg>
+                      <div class="card-body">
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                          <div class="btn-group">
+                          
+                            <button type="button" onclick="document.location= 'atualizar_usuario.php '" class="btn btn-sm btn-warning">Atualizar</button>
+                          </div>
+                          <small class="text-muted">9 mins</small>
+                        </div>
                       </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-        </main>
-      </div>
-    </div>
-    
-    
+              </main>
+              </div>
+            </div>
+        
+        
     
     </body>
 
