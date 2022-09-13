cb1b1efea1ea164fc657f55839a682808f225b66
Jonas Carvalho <jonasgabrielsc@gmail.com>
2022-09-13 17:18:03 -0300

    att

--- pages/produto.php
+++ pages/produto.php
@@ -5,19 +5,18 @@ $title = "Produto";
 include "../include/database.php";
 include "../include/header.php";
 include "../include/nave.php";
-$inf = "SELECT * FROM info_roupa JOIN produto ON id_produto = fk_id_produto WHERE fk_id_produto = 2";
+$inf = "SELECT * FROM info_roupa JOIN produto ON id_produto = fk_id_produto WHERE fk_id_produto = 25";
 $query = mysqli_query($con, $inf);
 $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
-var_dump($result[0]);
 ?>
 
 <body class="tabela">
     <div class="container-lg">
         <div class="row">
-            <div class="col-md-7">
+            <div class="col-md-5">
                 <div id="cafedama" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner sliderproduto">
-						<!--Pegar imagens dinamicamente-->
+                        <!--Pegar imagens dinamicamente-->
                         <div class="carousel-item active">
                             <img src="../Imagens/camisa1.png" class="d-block w-100" alt="...">
                         </div>
@@ -38,29 +37,31 @@ var_dump($result[0]);
                     </button>
                 </div>
             </div>
-            <div class="col-md-3" style="margin-top: 5mm;">
-                <?php
-					$produtos = $result[0];
-					echo "
-                    <h2>{$produtos['nome_produto']}<i class='bi bi-bookmark-plus'></i></h2>
-                    <h4>{$produtos['descricao']}</h4>";
-					
-					for ($i = 0; $i < 5; $i++) {
-						echo '<i class="bi bi-star-fill"></i>';
-					}
-                ?>
-                <div class='card card-body' style='width: 18rem;'>
-                    <h5 class='card-title text-center'>
-                        <p>Tamanho (BR)</p>
-                    </h5>
-                    <h6 class='card-subtitle'>Guia de tamanhos</h6>
 
+            <div class="col-md-3" style="margin-top: 5mm;">
+                <div class='card card-body' style='width: 30rem;'>
+                    <?php
+                    $produtos = $result[0];
+                    echo "
+                    <h2>{$produtos['nome_produto']}<i class='bi bi-bookmark-plus'></i></h2>
+                    <p>{$produtos['descricao']}</p>
+                    <div class='row'>
+                        <div class='col-md-8'>
+                            <h3 class='card-text'>R$ {$produtos['preco']}</h3>
+                        </div>
+                    </div>
+                    <p>";
+                    for ($i = 0; $i < 5; $i++) {
+                        echo '<i class="bi bi-star-fill"></i>';
+                    }
+                    ?>
+                    </p>
+
+                    <h5 class='card-title'>Guia de tamanhos</h5>
                     <?php
                     foreach ($result as $informacoes) {
-                            echo " 
-                        <p class='card-text'>
-                        <p>{$informacoes['tamanho']}</p>";
-						//add quantidade
+                        echo " 
+                        <h5 class='card-text'>{$informacoes['tamanho']} - Em estoque: {$informacoes['estoque']}</h5>";
                     }
                     ?>
                 </div>
