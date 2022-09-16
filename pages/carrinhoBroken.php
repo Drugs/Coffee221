bbfea31d27bc6b18bca2823857a3550adc749a08
Drugs <israelmadrugaps@gmail.com>
2022-09-16 17:32:14 -0300

    Rollback do carrinho

--- pages/carrinhoBroken.php
+++ pages/carrinhoBroken.php
@@ -38,18 +38,84 @@ echo "</pre>";
                 <div class="card-body p-4">
                     <div class="row ">
                         <div class="col">
-                            
                             <!---Titulo-->
                             <div class="d-flex justify-content-between align-items-center mb-4">
                                 <div>
                                     <h5 class="mb-1">Carrinho <i class="bi bi-cart3"></i></h5>
                                 </div>
                             </div>
+                            <!---Primeiro produto-->
+                            <div class="card mb-3" style="box-shadow: 10px 5px 5px ;">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between">
+                                        <div class="d-flex flex-row align-items-center">
+                                            <div>
+                                                <img src="../Imagens/caffeine2.jpg" class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
+                                            </div>
+                                            <div class="ms-3">
+                                                <h5>Camisa do café</h5>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center">
+                                            <button onclick="quant('item1', 'add', 'prod1')" type="button" class="mr-2 btn btn-dark">+</button>
+                                            <button onclick="quant('item1', 'sub', 'prod1')" type="button" class="ms-4 btn btn-dark">-</button>
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center">
+                                            <div style="width: 60px;">
+                                                <h5 id='item1' class="fw-normal mb-0" data-quantidade="1" data-preco="900"  data-length="4" data-prod="900">1</h5>
+                                            </div>
+<script>
+function quant(ide, fun, prod) {
+	let imprime = document.getElementById(ide)
+	let imprimeQnt = imprime.dataset.quantidade
+	let preco = imprime.dataset.preco
+	let dinheiro = document.getElementById(prod)
+	if (fun == 'add') {
+		imprimeQnt++
+	}
+	if (fun == 'sub') {
+		imprimeQnt--
+		if (imprimeQnt <= 0) {
+			imprimeQnt = 0
+		}
+	}
+	let precoNovo = imprimeQnt * preco
+	console.log(imprimeQnt * preco);
 
-                            
+	dinheiro.innerHTML = 'R$ ' + precoNovo
+	imprime.innerHTML = imprimeQnt
+	imprime.dataset.prod=precoNovo
+	imprime.dataset.quantidade = imprimeQnt
+	soma()
+}
+function soma(){
+    let valor=document.getElementById('item1')
+    let tamanho=valor.dataset.length
+    console.log(tamanho)
+    let aux
+    for (let i = 1; i <= tamanho; i++) {
+        aux=document.getElementById('item'+i)
+        //console.log(aux+imprimeQnt * preco)
+    } 
+    
+}</script>
+                                            
+
+				<div style="width: 80px;">
+					<h5 id='prod1' class="mb-0">R$ 900</h5>
+				</div>
+				<div style="width: 60px;">
+					<a><i class="bi bi-trash3-fill"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <!---Primeiro produto-->      
 	<?php
 		$anterior = 0; 
+
 		foreach($roupa as $vestido ){
 			$atual = $vestido['fk_id_info_produto']; 
 			if ($atual != $anterior){ 
@@ -133,6 +199,7 @@ echo "</pre>";
                                        
 
 
+
                             <!---Segundo produto-->
                             <div class="card mb-3" style="box-shadow: 10px 5px 5px ;">
                                 <div class="card-body">
@@ -150,8 +217,13 @@ echo "</pre>";
                                             <button  onclick="quant('item2', 'sub', 'prod2')" type="button" class="ms-4 btn btn-dark">-</button>
                                         </div>
                                         <div class="d-flex flex-row align-items-center">
+
+                                            <div style="width: 60px;">
+                                            <h5 id='item2' class="fw-normal mb-0" data-quantidade="1" data-preco="1000" data-prod="1000">1</h5>
+
                                             <div style="width: 80px;">
                                             <h5 id='item2' class="fw-normal mb-0" data-quantidade="1" data-preco="1000" >1</h5>
+
                                             </div>
                                             <div style="width: 90px;">
                                                 <h5 id='prod2' class="mb-0">$1000</h5>
@@ -183,7 +255,7 @@ echo "</pre>";
                                         </div>
                                         <div class="d-flex flex-row align-items-center">
                                             <div style="width: 60px;">
-                                            <h5 id='item3' class="fw-normal mb-0" data-quantidade="1" data-preco="1199">1</h5>
+                                            <h5 id='item3' class="fw-normal mb-0" data-quantidade="1" data-preco="1199" data-prod="1199">1</h5>
                                             </div>
                                             <div style="width: 80px;">
                                                 <h5 id='prod3' class="mb-0" >$1199</h5>
@@ -216,7 +288,7 @@ echo "</pre>";
                                         </div>
                                         <div class="d-flex flex-row align-items-center">
                                             <div style="width: 60px;">
-                                            <h5 id='item4' class="fw-normal mb-0" data-quantidade="1" data-preco="1799">1</h5>
+                                            <h5 id='item4' class="fw-normal mb-0" data-quantidade="1" data-preco="1799" data-prod="1799">1</h5>
                                             </div>
                                             <div style="width: 80px;">
                                                 <h5 class="mb-0" id='prod4'>$1799</h5>
@@ -231,31 +303,11 @@ echo "</pre>";
 
                             <div class="d-flex justify-content-between">
                                 <p class="mt-4 mb-2" >Total</p>
-                                <p class="mt-4 mb-2" id='total' >0</p>
 
-<script>
-	function quant(ide, fun, prod) {
-		let imprime = document.getElementById(ide)
-		let imprimeQnt = imprime.dataset.quantidade
-		let preco = imprime.dataset.preco
-		let dinheiro = document.getElementById(prod)
-		if (fun == 'add') {
-			imprimeQnt++
-		}
-		if (fun == 'sub') {
-			imprimeQnt--
-			if (imprimeQnt <= 0) {
-				imprimeQnt = 0
-			}
-		}
-		let precoNovo = imprimeQnt * preco
-		dinheiro.innerHTML = 'R$ ' + precoNovo
-		imprime.innerHTML = imprimeQnt
-		imprime.dataset.quantidade = imprimeQnt
-	}
-</script>
+                                <p class="mt-4 mb-2"  id='total' >0</p>
+
                             </div>
-
+                            <?php // gardar a variavel com os valores para somar e subtrair depois?> 
                             <button type="button" class="btn btn-info btn-block btn-lg" onclick="document.location= 'checkoutpage.php'">
                                 <span>Checkout</span>
                             </button>
@@ -270,3 +322,5 @@ echo "</pre>";
 <?php
 include '../include/footer.php';
 ?>
+
+
