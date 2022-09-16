a083ad81227fa7170041bd0401e57ac99663a55e
Bianca Nogueira <biancanogueiradasilva12@gmail.com>
2022-09-16 17:49:09 -0300

    get

--- pages/botanocarrinho.php
+++ pages/botanocarrinho.php
@@ -29,7 +29,7 @@ if(isset($_GET['id'])and $_GET['id'] !=0 ){ //se produto existe passa
 					if(isset($item['fk_id_info_produto'] ) and $item['fk_id_info_produto'] != 0){ //Alerta e tchau.
 					   echo '<div class="alert alert-warning" role="alert"> O item já está adicionado no carrinho!</div>';
 						}else{ //se nao está no carrinho coloca o item
-							$consulta3="INSERT INTO `item_de_carrinho`( `fk_id_info_produto`, `fk_id_carrinho`, `categoria`,`quantidade`) VALUES ({$id_produto},{$id_caro}, $_GET['categoria'], 1)";
+							$consulta3="INSERT INTO `item_de_carrinho`( `fk_id_info_produto`, `fk_id_carrinho`, `categoria`,`quantidade`) VALUES ({$id_produto},{$id_caro}, {$_GET['categoria']}, 1)";
 							$query = mysqli_query($con,$consulta3);
 							echo '<div class="alert alert-success" role="alert"> Item adicionado no carrinho com sucesso!</div>';
 						} 
