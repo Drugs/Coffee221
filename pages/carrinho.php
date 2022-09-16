c14feb3b929a279b51754e1dab4e904d0b282191
Drugs <israelmadrugaps@gmail.com>
2022-09-16 14:58:32 -0300

    Update carrinho.php

--- pages/carrinho.php
+++ pages/carrinho.php
@@ -7,7 +7,8 @@ include '../include/nave-site.php';
 //-------------------alimentos------------------------------------
 $consulta = "SELECT * FROM `carrinho` 
 join item_de_carrinho on item_de_carrinho.fk_id_carrinho = carrinho.id_carrinho 
-join info_alimento on info_alimento.id_info_alimento = item_de_carrinho.fk_id_info_produto join produto on produto.id_produto = info_alimento.fk_id_produto join galeria on galeria.fk_id_produto = produto.id_produto
+join info_alimento on info_alimento.id_info_alimento = item_de_carrinho.fk_id_info_produto join produto on produto.id_produto = info_alimento.fk_id_produto 
+join galeria on galeria.fk_id_produto = produto.id_produto
 WHERE item_de_carrinho.categoria ='alimento' 
 and carrinho.fk_id_pessoa = {$_SESSION['id_pessoa']}";
 $query = mysqli_query($con, $consulta);
@@ -18,7 +19,9 @@ echo "</pre>";
 //-------------------roupas------------------------------------
 $consulta = "SELECT * FROM `carrinho` 
 join item_de_carrinho on item_de_carrinho.fk_id_carrinho = carrinho.id_carrinho 
-join info_roupa on info_roupa.id_info_roupa = item_de_carrinho.fk_id_info_produto join produto on produto.id_produto = info_roupa.fk_id_produto  join galeria on galeria.fk_id_produto = produto.id_produto
+join info_roupa on info_roupa.id_info_roupa = item_de_carrinho.fk_id_info_produto 
+join produto on produto.id_produto = info_roupa.fk_id_produto  
+join galeria on galeria.fk_id_produto = produto.id_produto
 WHERE item_de_carrinho.categoria ='roupa' 
 and carrinho.fk_id_pessoa = {$_SESSION['id_pessoa']}";
 $query = mysqli_query($con, $consulta);
@@ -45,48 +48,56 @@ echo "</pre>";
 
                             
                             <!---Primeiro produto-->
-                            <div class="card mb-3" style="box-shadow: 10px 5px 5px ;">
-                                <div class="card-body">
-                                    <div class="d-flex justify-content-between">
+                            
                                         
                                     <?php
-                                    foreach($roupa as $vestido ){
-                                       echo"<div class='d-flex flex-row align-items-center'>
-                                                <div><img src = '../imagens/{$vestido['endereco']}' class='img-fluid rounded-3' alt='Shopping item' style='width: 65px;'>
-                                                </div></div>
-                                       <div class='mt-3'><h5>{$vestido['nome_produto']}</h5></div>"
-                                       
-                                       ;
-                                    }
+									$anterior = 0; 
+		foreach($roupa as $vestido ){
+			$atual = $vestido['fk_id_info_produto']; 
+			if ($atual != $anterior){ 
+				$subtotal = $vestido['quantidade'] * $vestido['preco'];
+				echo"<div class='card mb-3' style='box-shadow: 10px 5px 5px ;'>
+					<div class='card-body'>
+						<div class='d-flex justify-content-between'>
+							<div class='d-flex flex-row align-items-center'>
+								<div>
+									<img src = '../imagens/{$vestido['endereco']}' class='img-fluid rounded-3' alt='Shopping item' style='width: 65px;'>
+								</div>
+							</div>
+							<div class='mt-3'>
+								<h5>{$vestido['nome_produto']}</h5>
+							</div>
+							<div class='d-flex flex-row align-items-center'>
+								<button onclick=\"quant('item1', 'add', 'prod1')\" type='button' class='mr-2 btn btn-dark'>+</button>
+								<button onclick=\"quant('item1', 'sub', 'prod1')\" type='button' class='ms-4 btn btn-dark'>-</button>
+							</div>
+							<div class='d-flex flex-row align-items-center'>
+								<div style='width: 60px;'>
+									<h5 id='item1' class='fw-normal mb-0' data-quantidade='{$vestido['quantidade']}' data-preco='{$vestido['preco']}'>{$vestido['quantidade']}</h5>
+								</div>
+								<div style='width: 80px;'>
+									<h5 id='prod1' class='mb-0'>R$ {$subtotal}</h5>
+								</div>
+								<div style='width: 60px;'>
+									<a><i class='bi bi-trash3-fill'></i></a>
+								</div>
+							</div>            
+						</div>
+					</div>
+				</div>";
+			}
+			$anterior = $atual; 
+		}
 
 
                                     foreach($alimento as $consulta ){
                                        // echo"{$consulta['sabor']}";
  
-                                     }
+									}
                                     
                                         ?>
                                       
-                                        <div class="d-flex flex-row align-items-center">
-                                            <button onclick="quant('item1', 'add', 'prod1')" type="button" class="mr-2 btn btn-dark">+</button>
-                                            <button onclick="quant('item1', 'sub', 'prod1')" type="button" class="ms-4 btn btn-dark">-</button>
-                                        </div>
-                                        <div class="d-flex flex-row align-items-center">
-                                            <div style="width: 60px;">
-                                                <h5 id='item1' class="fw-normal mb-0" data-quantidade="1" data-preco="900">1</h5>
-                                            </div>
-
-                                            <div style="width: 80px;">
-                                                <h5 id='prod1' class="mb-0">R$ 900</h5>
-                                            </div>
-                                            <div style="width: 60px;">
-                                                <a><i class="bi bi-trash3-fill"></i></a>
-                                            </div>
-                                        </div>
-                                   
-                                    </div>
-                                </div>
-                            </div>
+                                       
 
 
                             <!---Segundo produto-->
@@ -189,69 +200,27 @@ echo "</pre>";
                                 <p class="mt-4 mb-2" >Total</p>
                                 <p class="mt-4 mb-2" id='total' >0</p>
 
-                                <script>
-                                                function quant(ide, fun, prod) {
-                                                    let imprime = document.getElementById(ide)
-                                                    let imprimeQnt = imprime.dataset.quantidade
-                                                    let preco = imprime.dataset.preco
-                                                    let dinheiro = document.getElementById(prod)
-                                                    if (fun == 'add') {
-                                                        imprimeQnt++
-                                                    }
-                                                    if (fun == 'sub') {
-                                                        imprimeQnt--
-                                                        if (imprimeQnt <= 0) {
-                                                            imprimeQnt = 0
-                                                        }
-                                                    }
-                                                    let precoNovo = imprimeQnt * preco
-                                                    console.log(imprimeQnt * preco);
-
-                                                    dinheiro.innerHTML = 'R$ ' + precoNovo
-                                                    imprime.innerHTML = imprimeQnt
-                                                    imprime.dataset.quantidade = imprimeQnt
-
-                                                    let novo = (imprimeQnt*preco+ imprimeQnt*preco+imprimeQnt*preco+imprimeQnt*preco)
-                                                                            
-                                                            console.log (novo)
-                                                            dinheiro.innerHTMAL=novo
-                                                            document.getElementById('total').innerHTML=(novo);
-                                                            }
-                                                            function soma(prod){
-
-                                                          document.getElementById(prod);
-
-                                                            console.log (prod+preco);
-
-                                                            }
-                                            </script>
-                                            <script>
-                                            function quant(ide, fun, prod) {
-                                                let imprime = document.getElementById(ide)
-                                                let imprimeQnt = imprime.dataset.quantidade
-                                                let preco = imprime.dataset.preco
-                                                let dinheiro = document.getElementById(prod)
-                                                if (fun == 'add') {
-                                                    imprimeQnt++
-                                                }
-                                                if (fun == 'sub') {
-                                                    imprimeQnt--
-                                                    if (imprimeQnt <= 0) {
-                                                        imprimeQnt = 0
-                                                    }
-                                                }
-                                                let precoNovo = imprimeQnt * preco
-                                                console.log(imprimeQnt * preco);
-
-                                                dinheiro.innerHTML = 'R$ ' + precoNovo
-                                                imprime.innerHTML = imprimeQnt
-                                                imprime.dataset.quantidade = imprimeQnt
-
-                                            }
-                                            
-
-                                        </script>
-
+<script>
+	function quant(ide, fun, prod) {
+		let imprime = document.getElementById(ide)
+		let imprimeQnt = imprime.dataset.quantidade
+		let preco = imprime.dataset.preco
+		let dinheiro = document.getElementById(prod)
+		if (fun == 'add') {
+			imprimeQnt++
+		}
+		if (fun == 'sub') {
+			imprimeQnt--
+			if (imprimeQnt <= 0) {
+				imprimeQnt = 0
+			}
+		}
+		let precoNovo = imprimeQnt * preco
+		dinheiro.innerHTML = 'R$ ' + precoNovo
+		imprime.innerHTML = imprimeQnt
+		imprime.dataset.quantidade = imprimeQnt
+	}
+</script>
                             </div>
 
                             <button type="button" class="btn btn-info btn-block btn-lg" onclick="document.location= 'checkoutpage.php'">
