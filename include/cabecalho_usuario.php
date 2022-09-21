a541120e47d27f8b15fbec44e46227ab8b404c72
Bianca Nogueira <biancanogueiradasilva12@gmail.com>
2022-09-21 14:32:48 -0300

    titulo

--- include/cabecalho_usuario.php
+++ include/cabecalho_usuario.php
@@ -33,7 +33,7 @@
   <link href="/coffee221/css/dashboard.css" rel="stylesheet">
 </head>
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="../index.php">Coffee 221</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="../index.php">The COFFE'JOIN</a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
