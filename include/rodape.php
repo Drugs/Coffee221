6ff54d05bebc886bc0875cb4a7256ba6ed1fb390
Jonas Carvalho <jonasgabrielsc@gmail.com>
2022-10-09 11:39:45 -0300

    Update rodape.php

--- include/rodape.php
+++ include/rodape.php
@@ -3,7 +3,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12 text-muted d-flex justify-content-md-center align-items-md-center">
-                        <span>© Copyright 2022. All Rights Reserved. Last Update: 2022-09-04</span>
+                        <span>© Copyright 2022. All Rights Reserved.</span>
                     </div>
                 </div>
             </div>
